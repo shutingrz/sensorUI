df5b608c25b44e0da73f1b0747671552db527e03
shu <shu@shutingrz.com>
2019-06-05 22:42:33 +0900

    deviceスキーマの変更

--- bin/sensors/db/sql/device.sql
+++ bin/sensors/db/sql/device.sql
@@ -1,6 +1,7 @@
 create table device(
+	device_name varchar(64) not null,
 	device_id varchar(32) primary key,
-	sensor_id integer,
-	user_hash varchar(32),
-	api_secret varchar(32)
+	sensor_id integer not null,
+	user_hash varchar(32) not null,
+	apikey varchar(32) not null
 );
