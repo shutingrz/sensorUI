f6a3a09a9e7c336bc9f010a2284cfd849e768b18
shu <shu@shutingrz.com>
2019-06-11 05:00:46 +0900

    デバイス登録までのテストケース作成

--- bin/tests/sensors_test.py
+++ bin/tests/sensors_test.py
@@ -96,6 +96,7 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
+        #login
         rv = self.app.get("/api/login", query_string=dict(
             username=testuser_data["name"],
             password=testuser_data["password"]
@@ -106,22 +107,89 @@ class TestUserControl(unittest.TestCase):
 
     def test_auth_test(self):
 
-        rv = self.app.get("/api/account/status")
-        print(rv.status_code)
+        testuser_data = {"name": "test_auth_test", "password": "testtest"}
 
+        #access login required page without session
+        rv = self.app.get("/api/account/status")
+        assert rv.status_code == 401
+
+        #register
+        rv = self.app.get("/api/register/user", query_string=dict(
+            username=testuser_data["name"],
+            password=testuser_data["password"]
+        ))
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+        #login
+        rv = self.app.get("/api/login", query_string=dict(
+            username=testuser_data["name"],
+            password=testuser_data["password"]
+        ))
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+        session = rv.headers["Set-Cookie"]
+        assert "session" in session
+
+        #access login required page with session
+        rv = self.app.get("/api/account/status", headers={"Cookie": session})
+        assert rv.status_code == 200
+
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
 
 
 
 class TestAccountControl(unittest.TestCase):
 
+    testuser_data = {"name": "test_account_control", "password": "testtest"}
+    session = None
+
     def setUp(self):        
         self.app = app.test_client()
 
+        #register
+        rv = self.app.get("/api/register/user", query_string=dict(
+            username=self.testuser_data["name"],
+            password=self.testuser_data["password"]
+        ))
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
 
-    def test_health(self):
-        rv = self.app.get("/api/")
-        assert b"Sensors" in rv.data
-    
+        #login
+        rv = self.app.get("/api/login", query_string=dict(
+            username=self.testuser_data["name"],
+            password=self.testuser_data["password"]
+        ))
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+        self.session = rv.headers["Set-Cookie"]
+
+
+    def test_device_register(self):
+        testdevice_data = {"device_name": "testdev", "sensor_type": "1"}
+
+        rv = self.app.get("/api/register/device", headers={"Cookie": self.session}, query_string=dict(
+                            device_name=testdevice_data["device_name"],
+                            sensor_type=testdevice_data["sensor_type"]
+            ))
+        
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+
+        #get device list
+        rv = self.app.get("/api/account/status", headers={"Cookie": self.session})
+        assert rv.status_code == 200
+
+        json_data = rv.get_json()
+        print(json_data)
+        assert json_data["header"]["status"] == "success"
+                    
+        
+
     
 
 
