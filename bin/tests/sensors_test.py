9e3a211bfc4012652c5ae2a6060dab0e206a6500
shu <shu@shutingrz.com>
2019-06-11 05:06:04 +0900

    テストケースの変数名変更

--- bin/tests/sensors_test.py
+++ bin/tests/sensors_test.py
@@ -26,80 +26,80 @@ class TestUserControl(unittest.TestCase):
         assert b"Sensors" in rv.data
     
     def test_user_isexist(self):
-        testuser_data = {"name": "test_user_isexist", "password": "testtest"}
+        testuser = {"name": "test_user_isexist", "password": "testtest"}
 
-        rv = self.app.get("/api/user/%s/isexist" % testuser_data["name"])
+        rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
 
     def test_user_register(self):
-        testuser_data = {"name": "test_user_register", "password": "testtest"}
+        testuser = {"name": "test_user_register", "password": "testtest"}
 
         #register
         rv = self.app.get("/api/register/user", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
         #isexist
-        rv = self.app.get("/api/user/%s/isexist" % testuser_data["name"])
+        rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == True
         
         #delete
-        rv = self.app.get("/api/admin/user/delete/%s" % testuser_data["name"])
+        rv = self.app.get("/api/admin/user/delete/%s" % testuser["name"])
         json_data = rv.get_json()
 
         #isexist
-        rv = self.app.get("/api/user/%s/isexist" % testuser_data["name"])
+        rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == False
 
 
     def test_admin_user_delete(self):
-        testuser_data = {"name": "test_admin_user_delete", "password": "testtest"}
+        testuser = {"name": "test_admin_user_delete", "password": "testtest"}
 
         #register
         rv = self.app.get("/api/register/user", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
         #isexist
-        rv = self.app.get("/api/user/%s/isexist" % testuser_data["name"])
+        rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == True
         
         #delete
-        rv = self.app.get("/api/admin/user/delete/%s" % testuser_data["name"])
+        rv = self.app.get("/api/admin/user/delete/%s" % testuser["name"])
         json_data = rv.get_json()
 
         #isexist
-        rv = self.app.get("/api/user/%s/isexist" % testuser_data["name"])
+        rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == False
         
 
     def test_user_login(self):
-        testuser_data = {"name": "test_user_login", "password": "testtest"}
+        testuser = {"name": "test_user_login", "password": "testtest"}
 
         #register
         rv = self.app.get("/api/register/user", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
         #login
         rv = self.app.get("/api/login", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
@@ -107,7 +107,7 @@ class TestUserControl(unittest.TestCase):
 
     def test_auth_test(self):
 
-        testuser_data = {"name": "test_auth_test", "password": "testtest"}
+        testuser = {"name": "test_auth_test", "password": "testtest"}
 
         #access login required page without session
         rv = self.app.get("/api/account/status")
@@ -115,16 +115,16 @@ class TestUserControl(unittest.TestCase):
 
         #register
         rv = self.app.get("/api/register/user", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
         #login
         rv = self.app.get("/api/login", query_string=dict(
-            username=testuser_data["name"],
-            password=testuser_data["password"]
+            username=testuser["name"],
+            password=testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
@@ -143,7 +143,7 @@ class TestUserControl(unittest.TestCase):
 
 class TestAccountControl(unittest.TestCase):
 
-    testuser_data = {"name": "test_account_control", "password": "testtest"}
+    testuser = {"name": "test_account_control", "password": "testtest"}
     session = None
 
     def setUp(self):        
@@ -151,16 +151,16 @@ class TestAccountControl(unittest.TestCase):
 
         #register
         rv = self.app.get("/api/register/user", query_string=dict(
-            username=self.testuser_data["name"],
-            password=self.testuser_data["password"]
+            username=self.testuser["name"],
+            password=self.testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
         #login
         rv = self.app.get("/api/login", query_string=dict(
-            username=self.testuser_data["name"],
-            password=self.testuser_data["password"]
+            username=self.testuser["name"],
+            password=self.testuser["password"]
         ))
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
@@ -169,11 +169,11 @@ class TestAccountControl(unittest.TestCase):
 
 
     def test_device_register(self):
-        testdevice_data = {"device_name": "testdev", "sensor_type": "1"}
+        testdevice = {"device_name": "testdev", "sensor_type": "1"}
 
         rv = self.app.get("/api/register/device", headers={"Cookie": self.session}, query_string=dict(
-                            device_name=testdevice_data["device_name"],
-                            sensor_type=testdevice_data["sensor_type"]
+                            device_name=testdevice["device_name"],
+                            sensor_type=testdevice["sensor_type"]
             ))
         
         json_data = rv.get_json()
