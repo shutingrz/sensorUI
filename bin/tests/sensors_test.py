faad618915902ac1bdd460c1fbe24a9ae362d8cf
shu <shu@shutingrz.com>
2019-06-11 00:33:52 +0900

    testを初期作成

---
+++ bin/tests/sensors_test.py
@@ -0,0 +1,19 @@
+import unittest
+import json
+from flask import Flask
+
+import app as sensors_app
+app = sensors_app.create_app()
+
+
+class TestSensorsAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_health(self):
+        rv = self.app.get("/api")
+        print(rv.data)
+
+if __name__ == '__main__':
+    unittest.main()
