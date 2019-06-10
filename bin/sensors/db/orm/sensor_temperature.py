bbab395cc66488c704fdb04deb46bf393991b111
shu <shu@shutingrz.com>
2019-06-11 01:58:12 +0900

    スキーマの調整

--- bin/sensors/db/orm/sensor_temperature.py
+++ bin/sensors/db/orm/sensor_temperature.py
@@ -7,7 +7,7 @@ class SensorTemperature(db.Model):
     __tablename__ = "sensor_temperature"
     id = db.Column(db.Integer(unsigned=True),
                    primary_key=True, autoincrement=True)
-    device_id = db.Column(db.String(32), db.ForeignKey("device.device_id"))
+    device_id = db.Column(db.String(32), db.ForeignKey("device.device_id"), nullable=False)
     time = db.Column(db.Integer, nullable=False)
     temperature = db.Column(db.DECIMAL, nullable=True)
     created = db.Column(db.DATETIME, default=datetime.now, nullable=False)
--- bin/sensors/db/orm/sensor_type.py
+++ bin/sensors/db/orm/sensor_type.py
@@ -5,7 +5,7 @@ class SensorType(db.Model):
 
     __tablename__ = "sensor_type"
     sensor_id = db.Column(db.Integer, primary_key=True)
-    sensor_name = db.Column(db.String(32))
+    sensor_name = db.Column(db.String(32), nullable=False)
 
     def __init__(self, sensor_id, sensor_name):
         self.sensor_id = sensor_id
