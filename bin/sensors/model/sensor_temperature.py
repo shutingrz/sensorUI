f5329af7beadc6a6a20261130700091bbf21d330
shu <shu@shutingrz.com>
2019-06-12 16:47:51 +0900

    温度記録を実装

--- bin/sensors/model/sensor_temperature.py
+++ bin/sensors/model/sensor_temperature.py
@@ -17,34 +17,64 @@ class SensorTemperatureModel(object):
 
         if (db and Temperature and Device) is None:
             return None, 100
-        
+
         try:
             devicelist = db.session.query(Device.device_id)\
-                                .filter(Device.user_hash == user_hash)\
-                                .filter(Device.device_id == device_id)\
-                                .all()
+                .filter(Device.user_hash == user_hash)\
+                .filter(Device.device_id == device_id)\
+                .all()
 
-            if not devicelist :
+            if not devicelist:
                 msg = "this device is not yours"
                 return msg, 110
 
             result = db.session.query(Temperature)\
-                                .filter(Temperature.device_id == device_id)\
-                                .order_by(desc(Temperature.time))\
-                                .limit(10)\
-                                .all()
-                                
+                .filter(Temperature.device_id == device_id)\
+                .order_by(desc(Temperature.time))\
+                .limit(10)\
+                .all()
+
         except Exception as exc:
             current_app.logger.critical("Unknown error: %s" % exc)
-        
+
         records = []
         for record in result:
             record_dict = {"time": record.time, "value": record.temperature}
             records.append(record_dict)
 
-
         return records, 0
 
-    def record(api_key, time, value):
-        msg = {"data": "unimplemented function"}
+    def record(self, api_key, time, value):
+
+        db = ORMUtil.initDB()
+        Temperature = ORMUtil.getSensorTemperatureORM()
+        Device = ORMUtil.getDeviceORM()
+
+        if (db and Temperature and Device) is None:
+            return None, 100
+
+        try:
+            deviceData = db.session.query(Device)\
+                .filter(Device.api_key == api_key)\
+                .first()
+
+            if not deviceData:
+                msg = "device data is not found"
+                return msg, 110
+
+            device_id = deviceData.device_id
+
+            db.session.add(Temperature(
+                device_id=device_id,
+                time=time,
+                temperature=value
+            ))
+
+            db.session.commit()
+
+        except Exception as exc:
+            current_app.logger.critical("Unknown error: %s" % exc)
+            return None, 199
+
+        msg = "ok"
         return msg, 0
--- bin/tests/sensors_test.py
+++ bin/tests/sensors_test.py
@@ -1,5 +1,6 @@
 import unittest
-import os,sys
+import os
+import sys
 import json
 import tempfile
 from flask import Flask
@@ -7,24 +8,26 @@ import subprocess
 import app as sensors_app
 import time
 
-#ここなんとかしたい・・・
+# ここなんとかしたい・・・
 tempDB = tempfile.mkstemp()
 tempDBPath = tempDB[1]
 dburl = "sqlite:///" + tempDBPath
-result = subprocess.run(["/bin/bash", "-c", "../scripts/init.sh %s" % tempDBPath])        
+result = subprocess.run(
+    ["/bin/bash", "-c", "../scripts/init.sh %s" % tempDBPath])
 time.sleep(1)
 
 app = sensors_app.create_app(dburl)
 
+
 class TestUserControl(unittest.TestCase):
 
-    def setUp(self):        
+    def setUp(self):
         self.app = app.test_client()
 
     def test_health(self):
         rv = self.app.get("/api/")
         assert b"Sensors" in rv.data
-    
+
     def test_user_isexist(self):
         testuser = {"name": "test_user_isexist", "password": "testtest"}
 
@@ -32,11 +35,10 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-
     def test_user_register(self):
         testuser = {"name": "test_user_register", "password": "testtest"}
 
-        #register
+        # register
         rv = self.app.get("/api/register/user", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -44,16 +46,15 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-        #isexist
+        # isexist
         rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == True
-
 
     def test_admin_user_delete(self):
         testuser = {"name": "test_admin_user_delete", "password": "testtest"}
 
-        #register
+        # register
         rv = self.app.get("/api/register/user", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -61,25 +62,24 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-        #isexist
+        # isexist
         rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == True
-        
-        #delete
+
+        # delete
         rv = self.app.get("/api/admin/user/delete/%s" % testuser["name"])
         json_data = rv.get_json()
 
-        #isexist
+        # isexist
         rv = self.app.get("/api/user/%s/isexist" % testuser["name"])
         json_data = rv.get_json()
         assert json_data["response"] == False
-        
 
     def test_user_login(self):
         testuser = {"name": "test_user_login", "password": "testtest"}
 
-        #register
+        # register
         rv = self.app.get("/api/register/user", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -87,7 +87,7 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-        #login
+        # login
         rv = self.app.get("/api/login", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -95,16 +95,15 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-
     def test_auth_test(self):
 
         testuser = {"name": "test_auth_test", "password": "testtest"}
 
-        #access login required page without session
+        # access login required page without session
         rv = self.app.get("/api/account/status")
         assert rv.status_code == 401
 
-        #register
+        # register
         rv = self.app.get("/api/register/user", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -112,7 +111,7 @@ class TestUserControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-        #login
+        # login
         rv = self.app.get("/api/login", query_string=dict(
             username=testuser["name"],
             password=testuser["password"]
@@ -123,7 +122,7 @@ class TestUserControl(unittest.TestCase):
         session = rv.headers["Set-Cookie"]
         assert "session" in session
 
-        #access login required page with session
+        # access login required page with session
         rv = self.app.get("/api/account/status", headers={"Cookie": session})
         assert rv.status_code == 200
 
@@ -131,19 +130,18 @@ class TestUserControl(unittest.TestCase):
         assert json_data["header"]["status"] == "success"
 
 
-
 class TestAccountControl(unittest.TestCase):
 
     testuser = {"name": "test_account_control", "password": "testtest"}
 
-    def setUp(self):        
+    def setUp(self):
         pass
 
     @classmethod
     def setUpClass(cls):
         cls.app = app.test_client()
-        
-        #register
+
+        # register
         rv = cls.app.get("/api/register/user", query_string=dict(
             username=cls.testuser["name"],
             password=cls.testuser["password"]
@@ -151,7 +149,7 @@ class TestAccountControl(unittest.TestCase):
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
-        #login
+        # login
         rv = cls.app.get("/api/login", query_string=dict(
             username=cls.testuser["name"],
             password=cls.testuser["password"]
@@ -161,41 +159,39 @@ class TestAccountControl(unittest.TestCase):
 
         cls.session = rv.headers["Set-Cookie"]
 
-
     def test_device_register(self):
         testdevice = {"device_name": "testdev", "sensor_type": "1"}
 
-        rv = self.app.get("/api/register/device", \
-            headers={"Cookie": self.session}, query_string=dict(
-                            device_name=testdevice["device_name"],
-                            sensor_type=testdevice["sensor_type"]
-            ))
+        rv = self.app.get("/api/register/device",
+                          headers={"Cookie": self.session}, query_string=dict(
+                              device_name=testdevice["device_name"],
+                              sensor_type=testdevice["sensor_type"]
+                          ))
 
         assert rv.status_code == 200
 
         json_data = rv.get_json()
-        
+
         assert json_data["header"]["status"] == "success"
 
-
-        #get device list
-        rv = self.app.get("/api/account/status", headers={"Cookie": self.session})
+        # get device list
+        rv = self.app.get("/api/account/status",
+                          headers={"Cookie": self.session})
         assert rv.status_code == 200
 
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
-                    
-        
+
     def test_temperature_view(self):
-        
+
         testdevice = {"device_name": "testdev2", "sensor_type": "1"}
 
-        #device register
-        rv = self.app.get("/api/register/device", \
-            headers={"Cookie": self.session}, query_string=dict(
-                            device_name=testdevice["device_name"],
-                            sensor_type=testdevice["sensor_type"]
-            ))
+        # device register
+        rv = self.app.get("/api/register/device",
+                          headers={"Cookie": self.session}, query_string=dict(
+                              device_name=testdevice["device_name"],
+                              sensor_type=testdevice["sensor_type"]
+                          ))
 
         assert rv.status_code == 200
 
@@ -205,15 +201,76 @@ class TestAccountControl(unittest.TestCase):
         device_id = json_data["response"]["device_id"]
         assert device_id is not None
 
-        #device view
-        rv = self.app.get("/api/device/temperature/%s" % device_id, \
-            headers={"Cookie": self.session})
-        
+        # device view
+        rv = self.app.get("/api/device/temperature/%s" % device_id,
+                          headers={"Cookie": self.session})
+
         assert rv.status_code == 200
 
         json_data = rv.get_json()
         assert json_data["header"]["status"] == "success"
 
+    def test_temperature_record(self):
+        from datetime import datetime
+
+        testdevice = {"device_name": "testdev3", "sensor_type": "1"}
+
+        # create testdata
+        test_records = []
+        now = datetime.now()
+        basetime = int(now.timestamp())
+
+        for i in range(0, 11):
+            record = {
+                "time": basetime + i*60,
+                "value": str(20+i)
+            }
+            test_records.append(record)
+
+        # device register
+        rv = self.app.get("/api/register/device",
+                          headers={"Cookie": self.session}, query_string=dict(
+                              device_name=testdevice["device_name"],
+                              sensor_type=testdevice["sensor_type"]
+                          ))
+
+        assert rv.status_code == 200
+
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+        api_key = json_data["response"]["api_key"]
+        device_id = json_data["response"]["device_id"]
+        assert api_key is not None
+        assert device_id is not None
+
+        # temperature record
+        for record in test_records:
+
+            rv = self.app.get("/api/record/temperature", query_string=dict(
+                api_key=api_key,
+                time=record["time"],
+                value=record["value"]
+            ))
+
+            assert rv.status_code == 200
+            json_data = rv.get_json()
+            assert json_data["header"]["status"] == "success"
+
+        # device view
+        rv = self.app.get("/api/device/temperature/%s" % device_id,
+                          headers={"Cookie": self.session})
+
+        assert rv.status_code == 200
+
+        json_data = rv.get_json()
+        assert json_data["header"]["status"] == "success"
+
+        # max 10 records check
+        res_records = json_data["response"]
+        assert len(res_records) < 11
+
+        print(json_data)
 
 
 if __name__ == '__main__':
