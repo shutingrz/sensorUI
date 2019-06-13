e7b83a3fabc6131832ebae97fbc7e6ba37ad66f4
shu <shu@shutingrz.com>
2019-06-14 01:35:39 +0900

    デバイス一覧表示画面を実装

--- bin/sensors/model/account.py
+++ bin/sensors/model/account.py
@@ -8,28 +8,24 @@ class AccountModel(object):
     def __init__(self):
         pass
 
+    # TODO account_statusをdevice_listに名称変更
+    def device_list(self, user_hash):
+        return self.account_status(user_hash)
+        
     def account_status(self, user_hash):
         db = ORMUtil.initDB()
         User = ORMUtil.getUserORM()
         Device = ORMUtil.getDeviceORM()
-        SensorType = ORMUtil.getSensorTypeORM()
 
 
-        if (db and User and Device and SensorType) is None:
+        if (db and User and Device) is None:
             return None, 100
 
         try:
             device_result = db.session.query(Device).filter(Device.user_hash == user_hash).all()
-            sensorType_result = db.session.query(SensorType).all()
         except Exception as exc:
             current_app.logger.critical("account_status: Unknown error: %s" % exc)
             return None, 199
-
-        #sensorの対応表取得
-        sensor_type = []
-        for sensorType in sensorType_result:
-            type_dict = {"id": sensorType.sensor_id, "name": sensorType.sensor_name}
-            sensor_type.append(type_dict)
 
         devices = []
         
@@ -37,7 +33,7 @@ class AccountModel(object):
             device_dict = { "device_name": device.device_name,
                             "device_id": device.device_id,
                             "sensor_type": device.sensor_type,
-                            "api_key": device.api_key}
+                            "api_key": device.api_key,}
             
             devices.append(device_dict)
 
@@ -58,6 +54,15 @@ class AccountModel(object):
         api_key = Util.generateRandomString(32)
 
         try:
+            # 存在するセンサータイプが指定されているか
+            sensorType = db.session.query(SensorType)\
+                .filter(SensorType.sensor_id == sensor_type)\
+                .first()
+
+            if not sensorType:
+                return None, 99
+
+            # デバイス登録
             db.session.add(Device(
                 device_id=device_id,
                 device_name=device_name,
